ac576f9286a86d3f3523bfd764b709ba1b83a3da
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2019-01-18 22:54:55 -0500

    Update error handling.

--- stratus/app.py
+++ stratus/app.py
@@ -12,7 +12,7 @@ _HERE = os.path.dirname(__file__)
 _SETTINGS = os.path.join(_HERE, 'settings.ini')
 
 def render_exception( ex: Exception ):
-    return Response(response=json.dumps({ 'error': str(ex), 'message': getattr(ex, 'message', repr(ex))} ), status=401, mimetype="application/json")
+    return Response(response=json.dumps({ 'message': getattr(ex, 'message', repr(ex)), "code": 500 } ), status=500, mimetype="application/json")
 
 app = connexion.FlaskApp("stratus", specification_dir='api/', debug=True )
 app.add_error_handler( 500, render_exception )
@@ -21,6 +21,7 @@ settings = os.environ.get('FLASK_SETTINGS', _SETTINGS)
 if settings is not None:
     config_file = Config(settings)
     app.app.config.update( config_file.get_map('flask') )
+    app.app.config.update( { 'PROPAGATE_EXCEPTIONS': False } )
 
 # if blueprints is not None:
 #     for bp in blueprints:
