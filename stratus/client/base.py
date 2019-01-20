28047c43e17ac18e079c874931e1d7eb2ca8ce8f
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2019-01-19 23:20:57 -0500

    Add client base

---
+++ stratus/client/base.py
@@ -0,0 +1,14 @@
+from pyswagger import App, Security
+from pyswagger.contrib.client.requests import Client
+from pyswagger.spec.v2_0.objects import Operation
+from flask import Response
+
+class ServerProxy:
+    def __init__(self, openapi_spec: str ):
+        self.app = App._create_( openapi_spec )
+        self.client = Client()
+
+    def request(self, method: str, **kwargs ) -> Response:
+        op: Operation = self.app.op[ method ]
+        response = self.client.request( op(**kwargs) )
+        return response
--- stratus/client/hpda.py
+++ stratus/client/hpda.py
@@ -1,13 +1,6 @@
-from pyswagger import App, Security
-from pyswagger.contrib.client.requests import Client
-from pyswagger.spec.v2_0.objects import Operation
-from stratus.util.parsing import HtmlCodec
-# from stratus.client.base import Client
-from pyswagger.utils import jp_compose
+from stratus.client.base import ServerProxy
 
-app = App._create_('http://127.0.0.1:5000/hpda/swagger.json')
-
-client = Client()
+server = ServerProxy( 'http://127.0.0.1:5000/hpda/swagger.json' )
 
 variables=[ dict( name='tas:v0', uri='collection://merra2')  ]
 bounds = [ dict( axis='lat', start=-50, end=50, crs='values' ) ]
@@ -15,15 +8,12 @@ domains=[ dict( name='d0', bounds=bounds) ]
 operations=[ dict( name='ave', axis='t', input='v0', domain='d0' )  ]
 request=dict(variables=variables, domains=domains, operations=operations )
 
-op: Operation = app.op['exe']
-response = client.request( op(request=request) )
+response = server.request( 'exe', request=request )
 print( response.data )
 
-op: Operation = app.op['exeStat']
-response = client.request( op( id=response.data['id'] ) )
+response = server.request( 'exeStat', id=response.data['id'] )
 print( response.data )
 
-op: Operation = app.op['exeKill']
-response = client.request( op( id=response.data['id'] ) )
+response = server.request( 'exeKill', id=response.data['id'] )
 print( response.data )
 
