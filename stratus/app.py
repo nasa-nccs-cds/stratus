fa83cdc1e64e9f6e79e9401ec5a0d7bd1b7e9f38
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2019-01-22 13:17:42 -0500

    Add Stratus app.

--- stratus/app.py
+++ stratus/app.py
@@ -1,22 +1,9 @@
-import os
-# from stratus.views import blueprints
-from connexion.resolver import RestyResolver
-# from stratus.views.blueprints import SwaggerBlueprint, JsonBlueprint
 from connexion.resolver import Resolver
 from connexion.operations import AbstractOperation
-
 import os, traceback
 from flask import Flask, Response
 import connexion, json, logging
-from stratus.util.config import Config, StratusLogger, STRATUS_CONFIG
-logger = StratusLogger.getLogger()
-_HERE = os.path.dirname(__file__)
-_SETTINGS = os.path.join(_HERE, 'settings.ini')
-
-def render_server_error( ex: Exception ):
-    print( str( ex ) )
-    traceback.print_exc()
-    return Response(response=json.dumps({ 'message': getattr(ex, 'message', repr(ex)), "code": 500 } ), status=500, mimetype="application/json")
+from stratus.util.config import Config, StratusLogger
 
 class StratusResolver(Resolver):
 
@@ -29,26 +16,38 @@ class StratusResolver(Resolver):
         router_controller = operation.router_controller if operation.router_controller else self.default_handler_package
         return '{}.{}'.format(router_controller, operation_id)
 
-app = connexion.FlaskApp("stratus", specification_dir='api/', debug=True )
-app.add_error_handler( 500, render_server_error )
-flask: Flask = app.app
-flask.register_error_handler( TypeError, render_server_error )
+class StratusApp:
+    HERE = os.path.dirname(__file__)
+    SETTINGS = os.path.join( HERE, 'settings.ini')
 
-settings = os.environ.get('FLASK_SETTINGS', _SETTINGS)
-config_file = Config(settings)
-app.app.config.update( config_file.get_map('flask') )
-stratus_parms = config_file.get_map('stratus')
-api = stratus_parms.get( 'API', None )
-if api is None: raise Exception( "Missing required stratus parameter in settings.ini: 'API'" )   #  'hpda1.yaml
-handler = stratus_parms['HANDLER']
-if handler is None: raise Exception( "Missing required stratus parameter in settings.ini: 'HANDLER'" ) # 'stratus.handlers.hpda1'
+    def __init__(self):
+        self.logger = StratusLogger.getLogger()
+        self.app = connexion.FlaskApp("stratus", specification_dir='api/', debug=True )
+        self.app.add_error_handler( 500, self.render_server_error )
+        self.app.app.register_error_handler( TypeError, self.render_server_error )
+        settings = os.environ.get('FLASK_SETTINGS', self.SETTINGS )
+        config_file = Config(settings)
+        self.app.app.config.update( config_file.get_map('flask') )
+        self.parms = config_file.get_map('stratus')
+        api = self.getParameter( 'API' )
+        handler = self.getParameter( 'HANDLER' )
+        self.app.add_api( api + ".yaml", resolver=StratusResolver( handler ) )
 
-# if blueprints is not None:
-#     for bp in blueprints:
-#         app.app.register_blueprint(bp)
-#         print( "Register blueprint: " + str(bp))
+    def run(self):
+        port = self.getParameter( 'PORT', 5000 )
+        return self.app.run( int( port ) )
+
+    def getParameter(self, name: str, default = None ) -> str:
+        parm = self.parms.get( name, default )
+        if parm is None: raise Exception( "Missing required stratus parameter in settings.ini: " + name )
+        return parm
+
+    def render_server_error( ex: Exception ):
+        print( str( ex ) )
+        traceback.print_exc()
+        return Response(response=json.dumps({ 'message': getattr(ex, 'message', repr(ex)), "code": 500 } ), status=500, mimetype="application/json")
 
 
-
-app.add_api( api + ".yaml", resolver=StratusResolver( handler ) )
-app.run( 5000 )
+if __name__ == "__main__":
+    app = StratusApp()
+    app.run()
