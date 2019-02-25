708b3de97beffd81d51d8baf49f5e81ee9e3d365
Thomas Maxwell <thomas.maxwell@nasa.gov>
2019-02-25 18:46:39 -0500

    Add wps to rest

--- stratus/handlers/rest/wps/app.py
+++ stratus/handlers/rest/wps/app.py
@@ -20,13 +20,15 @@ class WPSStratusApp(StratusApp):
             if request.method == 'GET':
                 requestArgs = { key.lower():value for key,value in request.args.items() }
                 rType: str = requestArgs.get("request",None)
+                clients = self.getClients( "edas.")
+                assert len(clients), "Can't find edas client"
                 assert rType is not None, "Missing 'Request' argument"
 #                if rType.lower() == "execute":
 #                    rInputs: str = requestArgs.get("datainputs", None)
-            if request.method == 'POST':
-                requestArgs = {key.lower(): value for key, value in request.form.items()}
-                rType: str = requestArgs.get("request", None)
-                assert rType is not None, "Missing 'Request' argument"
+#             if request.method == 'POST':
+#                 requestArgs = {key.lower(): value for key, value in request.form.items()}
+#                 rType: str = requestArgs.get("request", None)
+#                 assert rType is not None, "Missing 'Request' argument"
 #                if rType.lower() == "execute":
 #                    rInputs: str = requestArgs.get("datainputs", None)
                 return "Hello!"
