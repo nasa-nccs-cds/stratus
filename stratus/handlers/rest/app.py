0290a5c5cf15076e2af25da48c0196b3d8c6a195
Thomas Maxwell <thomas.maxwell@nasa.gov>
2019-02-26 13:15:48 -0500

    Add RestAPI

--- stratus/handlers/rest/app.py
+++ stratus/handlers/rest/app.py
@@ -1,12 +1,28 @@
 from typing import List, Dict, Any, Sequence, BinaryIO, TextIO, ValuesView, Optional
 from stratus.handlers.client import StratusClient
-import os, traceback
-from flask import Flask, Response
-import json, logging
+import os, traceback, abc
+from flask import Flask, Response, Blueprint, render_template
+import json, logging, importlib
 from functools import partial
 from stratus.util.config import Config, StratusLogger
 from flask_sqlalchemy import SQLAlchemy
 from stratus.handlers.app import StratusCore
+
+class RestAPIBase:
+    __metaclass__ = abc.ABCMeta
+
+    def __init__( self, name, **kwargs ):
+        self.logger = StratusLogger.getLogger()
+        self.parms = kwargs
+        self.name = name
+
+    @abc.abstractmethod
+    def _createBlueprint( self ): pass
+
+    def instantiate( self, app ):
+        bp: Blueprint = self._createBlueprint()
+        self.logger.info( f"Instantiating API: {bp.name}" )
+        app.register_blueprint( bp )
 
 class StratusApp(StratusCore):
 
@@ -29,7 +45,20 @@ class StratusApp(StratusCore):
         app = Flask( "stratus", instance_relative_config=True )
         app.config.from_mapping( parms )
         app.register_error_handler( TypeError, self.render_server_error )
+        self.addApis( app )
         return app
+
+    def addApis(self, app ):
+        apiList = self.parm("API","stratus").split(",")
+        for apiName in apiList:
+            try:
+                package_name = f"stratus.handlers.rest.{apiName}.app"
+                module = importlib.import_module( package_name )
+                constructor = getattr( module, "RestAPI" )
+                rest_api: StratusRestAPI = constructor( apiName )
+                rest_api.instantiate( app )
+            except Exception as err:
+                self.logger.error( f"Error instantiating api {apiName}: {str(err)}")
 
     @staticmethod
     def render_server_error( ex: Exception ):
@@ -38,5 +67,7 @@ class StratusApp(StratusCore):
         return Response(response=json.dumps({ 'message': getattr(ex, 'message', repr(ex)), "code": 500, "id": "", "status": "error" } ), status=500, mimetype="application/json")
 
 if __name__ == "__main__":
-    app = StratusApp()
+    HERE = os.path.dirname(os.path.abspath(__file__))
+    SETTINGS_FILE = os.path.join(HERE, "test_settings.ini")
+    app = StratusApp( settings=SETTINGS_FILE )
     app.run()
--- stratus/handlers/rest/wps/app.py
+++ stratus/handlers/rest/wps/app.py
@@ -1,28 +1,22 @@
 from stratus.handlers.rest.app import StratusApp
-from flask import Flask, Response, request
+from flask import Flask, Response, request, Blueprint
 from typing import List, Dict, Any, Sequence, BinaryIO, TextIO, ValuesView, Optional
-import os
-HERE = os.path.dirname(os.path.abspath(__file__))
-SETTINGS_FILE = os.path.join( HERE, "wps_settings.ini" )
+from stratus.handlers.rest.app import RestAPIBase
+import os, abc
 
-class WPSStratusApp(StratusApp):
+class RestAPI(RestAPIBase):
 
-    def __init__( self, **kwargs ):
-        StratusApp.__init__( self, **kwargs )
+    def _createBlueprint( self ) -> Blueprint:
+        bp = Blueprint( self.name, __name__, url_prefix=f'/{self.name}' )
 
-    def create_app(self, parms: Dict):
-        app = Flask("stratus", instance_relative_config=True)
-        app.config.from_mapping(parms)
-        app.register_error_handler(TypeError, self.render_server_error)
-
-        @app.route('/wps', methods=('GET', 'POST'))
+        @bp.route('/exe', methods=('GET', 'POST'))
         def wps():
             if request.method == 'GET':
                 requestArgs = { key.lower():value for key,value in request.args.items() }
                 rType: str = requestArgs.get("request",None)
-                clients = self.getClients( "edas.")
-                assert len(clients), "Can't find edas client"
-                assert rType is not None, "Missing 'Request' argument"
+#                clients = self.getClients( "edas.")
+#                assert len(clients), "Can't find edas client"
+#                assert rType is not None, "Missing 'Request' argument"
 #                if rType.lower() == "execute":
 #                    rInputs: str = requestArgs.get("datainputs", None)
 #             if request.method == 'POST':
@@ -32,12 +26,8 @@ class WPSStratusApp(StratusApp):
 #                if rType.lower() == "execute":
 #                    rInputs: str = requestArgs.get("datainputs", None)
                 return "Hello!"
-
-        return app
-
+        return bp
 
 
 
-if __name__ == "__main__":
-    app = WPSStratusApp( settings=SETTINGS_FILE )
-    app.run()
+
