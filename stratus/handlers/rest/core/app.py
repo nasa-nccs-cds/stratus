31797fedc8c5907ae69cf5d03bb8e0854ebdc500
Thomas Maxwell <thomas.maxwell@nasa.gov>
2019-02-26 18:36:30 -0500

    Update rest service

--- stratus/handlers/rest/core/app.py
+++ stratus/handlers/rest/core/app.py
@@ -11,17 +11,24 @@ class RestAPI(RestAPIBase):
 
         @bp.route('/exe', methods=('GET', 'POST'))
         def exe():
-            if request.method == 'GET':
-                requestArgs = { key.lower():value for key,value in request.args.items() }
-                requestSpec: str = requestArgs.get("request",None)
-                client = self.core.getClient( "edas")
-                requestDict: Dict = self.jsonRequest( requestSpec )
-                if requestDict["status"] == "error": return self.jsonResponse( requestDict, 400 )
-                task = client.request(  "exe", request=requestDict )
-                return self.jsonResponse( dict( status="executing", id=task.id ) )
+            if request.method == 'POST':    requestSpec: str = request.form["request"]
+            else:                           requestSpec: str = request.get("request",None)
+            client = self.core.getClient( "edas")
+            requestDict: Dict = self.jsonRequest( requestSpec )
+            task = client.request(  "exe", request=requestDict )
+            return self.jsonResponse( dict( status="executing", id=task.id ) )
 
 
-    # response = server.request( "stat", id=response['id'] )
+        @bp.route('/capabilities', methods=('GET'))
+        def exe():
+            requestSpec: str = request.get("request", None)
+
+            client = self.core.getClient("edas")
+            requestDict: Dict = self.jsonRequest(requestSpec)
+            task = client.request("exe", request=requestDict)
+            return self.jsonResponse(dict(status="executing", id=task.id))
+
+                # response = server.request( "stat", id=response['id'] )
     # print( response )
     #
     # response = server.request( "kill", id=response['id'] )
@@ -35,7 +42,7 @@ class RestAPI(RestAPIBase):
 #                 assert rType is not None, "Missing 'Request' argument"
 #                if rType.lower() == "execute":
 #                    rInputs: str = requestArgs.get("datainputs", None)
-                return "Hello!"
+
         return bp
 
 
