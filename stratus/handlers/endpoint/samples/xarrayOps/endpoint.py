be28d191f03065c55c2950f59f88908a2e4521da
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2019-07-25 12:32:06 -0400

    Add executor sample

--- stratus/handlers/endpoint/samples/xarrayOps/endpoint.py
+++ stratus/handlers/endpoint/samples/xarrayOps/endpoint.py
@@ -2,6 +2,7 @@ from stratus_endpoint.handler.base import TaskHandle, TaskResult
 from typing import List, Dict, Any, Sequence, BinaryIO, TextIO, ValuesView, Optional, Callable
 from stratus_endpoint.util.config import StratusLogger
 from stratus_endpoint.handler.execution import Executable, ExecEndpoint
+import xarray as xa
 import abc
 
 
@@ -26,7 +27,26 @@ class XaOpsEndpoint(ExecEndpoint):
 
 class XaOpsExecutable(Executable):
 
-    @abc.abstractmethod
     def execute(self, **kwargs) -> TaskResult:
         print( f"Executing request {self.request}" )
-        return TaskResult( kwargs )
+        result_datasets: List[xa.Dataset] = []
+        for input in self.inputs:
+            dset : xa.Dataset = input.getDataset()
+            result_datasets.append( dset.mean( dim='time') )
+        return TaskResult( kwargs, result_datasets )
+
+
+    def execute1(self, **kwargs) -> TaskResult:
+        print( f"Executing request {self.request}" )
+        result_datasets: List[xa.Dataset] = []
+        for input in self.inputs:
+            result_vars = {}
+            dset : xa.Dataset = input.getDataset()
+            for id, var in dset.data_vars.items():
+                result_vars[id] = self.op( var )
+            result_datasets.append( xa.Dataset( result_vars, dset.coords, dset.attrs ) )
+        return TaskResult( kwargs, result_datasets )
+
+    def op(self, var: xa.DataArray ) -> xa.DataArray:
+        operation = self.request['operation']
+        return var.mean( dim='time' )
