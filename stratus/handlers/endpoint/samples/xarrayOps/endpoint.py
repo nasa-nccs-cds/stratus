bc55523e255a82adedc9747d3c8e53cab8e24a5c
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2019-07-26 13:08:15 -0400

    Add sample documentation

--- stratus/handlers/endpoint/samples/xarrayOps/endpoint.py
+++ stratus/handlers/endpoint/samples/xarrayOps/endpoint.py
@@ -7,27 +7,70 @@ import abc
 
 
 class XaOpsEndpoint(ExecEndpoint):
-    __metaclass__ = abc.ABCMeta
+
+    """
+        This class is used to implement the capabilities of the Endpoint.
+    """
 
     @abc.abstractmethod
     def createExecutable( self, requestSpec: Dict, inputs: List[TaskResult] = None, **kwargs ) -> Executable:
+        """
+            Factory method for Executable objects.
+            Creates an Executable for each analytics operation
+
+            Parameters:
+            requestSpec (Dict):         Dict which defines the analytics operation
+            inputs: (List[TaskResult]): Inputs from other operations in the workflow
+
+            Returns:
+            Executable: A Executable object which execute the operation.
+            """
         return XaOpsExecutable( requestSpec, inputs, **kwargs )
 
-    def shutdown(self, **kwargs ):
+    def init( self ):
+        """
+            Used to startup analytic resources such as scheduler, worker nodes, database, etc.
+            """
         return
 
-    @abc.abstractmethod
+    def shutdown(self, **kwargs ):
+        """
+            Used to shut down and release resources that were alloacted in the init method.
+            """
+        return
+
     def capabilities(self, type: str, **kwargs ) -> Dict:
+        """
+            Used to return metadata describing the capabilities of the Endpoint.
+            The only required response is the definition the Endpoint Address (epa) for this Endpoint.
+            The epa is used to route operation requests to the an Endpoint that can process them.
+
+            Parameters:
+                type (str):  Optionally allows the definition of various types of capabilty requests.
+
+            Returns:
+                  Dict:  metadata describing the capabilities of the Endpoint.
+            """
         return dict( epas = [ "xop*"] )
 
-    @abc.abstractmethod
-    def init( self ):
-        return
 
 
 class XaOpsExecutable(Executable):
 
+    """
+        This class is used to implement a single operation.
+    """
+
     def execute(self, **kwargs) -> TaskResult:
+        """
+            Executes the operation.
+            Creates an Executable for each analytics operation
+            The operation request is available as self.request.
+            The operation inputs are available as self.inputs.
+
+            Returns:
+            TaskResult: The result of the operation.
+            """
         print( f"Executing request {self.request}" )
         inputSpec = self.request['input']
         dset: xa.Dataset = xa.open_dataset( inputSpec['uri'] )
@@ -38,6 +81,9 @@ class XaOpsExecutable(Executable):
         return TaskResult( kwargs, [ resultDataset ] )
 
     def operate(self, vid: str, variable: xa.DataArray )-> Dict[str,xa.DataArray] :
+        """
+            Convenience method defined for this particular operation
+        """
         opSpecs = self.request['operation']
         result_arrays: Dict[str,xa.DataArray] = {}
         for opSpec in opSpecs:
